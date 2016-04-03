8bfd7761278ce255eae6b7d1ce8b5bb754449639
Timur Kiyivinski <deathseclipse@gmail.com>
2016-04-03 08:23:33 +0800

    [Done] User column migration additions

---
+++ database/migrations/2016_04_03_000636_add_user_columns.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('occupation');
+            $table->text('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('occupation');
+            $table->dropColumn('description');
+        });
+    }
+}
