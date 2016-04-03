e26ddc6b06fc29ff17dc7d686d0502c080d334bd
Timur Kiyivinski <deathseclipse@gmail.com>
2016-04-03 11:41:39 +0800

    [WIP] Management

---
+++ app/Http/Controllers/Management/EventController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Management;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class EventController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+}
